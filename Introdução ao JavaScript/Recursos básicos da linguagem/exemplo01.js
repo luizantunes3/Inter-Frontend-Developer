1ec7336760a92cb0e7d9dc2ca3aa207cbba940cb
Luiz Antunes <luiz.antunes3@fatec.sp.gov.br>
2021-12-15 09:34:10 -0300

    Inclusão dos conteúdos das aulas de Introdução ao Javascript

---
+++ Introdução ao JavaScript/Recursos básicos da linguagem/exemplo01.js
@@ -0,0 +1,18 @@
+//Exemplo de comentário
+/* Exemplo de comentário em bloco */
+
+//exemplo de variável
+var A = 0;
+const B = 0;
+
+function soma(a, b){
+    console.log(a+b);
+    return a+b;
+}
+
+soma(5,5);
+
+
+
+
+
---
+++ Sintaxe Básica em JavaScript/script.js
@@ -0,0 +1,8 @@
+//Tipos de variáveis
+var a = true;
+var b;
+let c;
+const d = 2;
+
+var array = ['Luiz', 'Fernando'];
+alert(array);
