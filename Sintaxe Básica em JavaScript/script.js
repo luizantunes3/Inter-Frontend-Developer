5ef51d3e22830554c9b1075ea2f4948808303ab8
Luiz Antunes <luiz.antunes3@fatec.sp.gov.br>
2021-12-20 18:02:48 -0300

    Atualização e Inclusão de alguns arquivos javascript

--- Sintaxe Básica em JavaScript/script.js
+++ Sintaxe Básica em JavaScript/script.js
@@ -4,5 +4,55 @@ var b;
 let c;
 const d = 2;
 
-var array = ['Luiz', 'Fernando'];
-alert(array);
+
+function exemploObjeto(){
+    var objeto = new Object({
+        'Cadeira':'10',
+        'Mesa':'10'
+    });
+
+    alert(objeto.Mesa);
+}
+
+async function exemploPromessa(){
+ const myPromisse = new Promisse((resolve, reject) => {
+     window.setTimeout(() => {
+         resolve('resolvida!');
+     }, 2000);
+ });
+
+ const resolved = await myPromisse
+    .then((result ) => result + ' passando pelo then')
+    
+    return resolved;
+}
+
+function exemploErro(){
+    const exemploErro = new Error('Ops, houve um problema');
+    exemploErro.name = 'Problema001';
+    throw exemploErro;
+}
+
+function exemploSet(){
+    const mySet = new Set();
+
+    mySet.add(2);
+    mySet.add(5);
+    mySet.add(10);
+
+    alert(mySet.has(2));
+}
+
+function exemploMap(){
+    const myMap = new Map();
+     myMap.set('ContaCC', '2.500');
+     myMap.set('ContaPoupança', '100.00');
+
+     alert(myMap.get('ContaPoupança'));
+
+}
+
+function exemploArray (){
+    var array = ['Luiz', 'Fernando'];
+    alert(array);
+}
